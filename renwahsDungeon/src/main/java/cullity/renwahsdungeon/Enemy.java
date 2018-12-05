6dc01dc83f8accf5effdeb2635c6a166ed594cb9
shawnb58 <shawnb58@BHSCUR149013ITW.kprdsb.schools.local>
2018-12-05 14:25:39 -0500

    made basic objects and extensions

---
+++ renwahsDungeon/src/main/java/cullity/renwahsdungeon/Enemy.java
@@ -0,0 +1,67 @@
+/*
+ * Made By: Shawn Benedict
+ * Date: Dec 5, 2018
+ * Made to
+ */
+package cullity.renwahsdungeon;
+
+/**
+ *
+ * @author shawnb58
+ */
+public class Enemy {
+     private double strength;
+    private double health;
+    //private boolean employed;
+    private String type;//class
+    private double defense;
+    
+
+    public Enemy() {
+        strength = 10;
+        health = 100;
+        defense = 10;
+        type = "slime";
+       
+
+    }
+
+    public Enemy(double s,double h, double d, String t) {
+        strength = s;
+        health = h;
+        defense = d;
+        type = t; 
+    }
+
+    public double getStrength() {
+        return strength;
+    }
+
+    public double getHealth() {
+        return health;
+    }
+
+    public double getDefense() {
+        return defense;
+    }
+
+    public String getType() {
+        return type;
+    }
+    
+    public void setStrength(double b) {
+        strength=b;
+    }
+
+    public void setHealth(double h) {
+        health = h;
+    }
+
+    public void setDefense(double d) {
+        defense=d;
+    }
+
+    public void setType(String t) {
+        type = t;
+    }
+}
