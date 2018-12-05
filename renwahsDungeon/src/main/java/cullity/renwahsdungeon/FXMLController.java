ca0b77ec0b1a30f6a6525a77e02d60f8f3b43429
conman1005 <connercullity@live.ca>
2018-12-05 14:18:53 -0500

    Started the uner database

--- renwahsDungeon/src/main/java/cullity/renwahsdungeon/FXMLController.java
+++ renwahsDungeon/src/main/java/cullity/renwahsdungeon/FXMLController.java
@@ -11,11 +11,19 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextInputDialog;
 
 public class FXMLController implements Initializable {
+    
+    data dbs = new data();
+    
+    String file = "database.raf";
 
     Alert alert = new Alert(AlertType.INFORMATION);
+    
+    @FXML
+    private ListView lstSaves;
 
     @FXML
     private void btnPlay(ActionEvent event) {
@@ -31,7 +39,7 @@ public class FXMLController implements Initializable {
 
         Optional<String> result = dialog.showAndWait();
         if (result.isPresent()) {
-            if (result.toString().length() > 15) {
+            if (result.get().length() > 15) {
                 alert.setTitle("Error");
                 alert.setHeaderText("Error");
                 alert.setContentText("Please use 15 characters or less");
@@ -39,7 +47,11 @@ public class FXMLController implements Initializable {
                 alert.showAndWait();
                 result = dialog.showAndWait();
             } else {
+                dbs.setName(result.get());
+                dbs.setLevel(0);
+                dbs.save(file, lstSaves.getItems().size() + 1);
                 
+                lstSaves.getItems().add(result.get());
             }
         }
     }
@@ -51,6 +63,9 @@ public class FXMLController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        for (int i = 0; i < dbs.numRecord(file); i++) {
+            dbs.open(file, i);
+            lstSaves.getItems().add(dbs.getName());
+        }
     }
 }
