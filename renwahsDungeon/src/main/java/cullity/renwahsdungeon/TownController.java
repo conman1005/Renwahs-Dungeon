bbb1121132fae2bd3761f2a7eef6d0ada298e45b
shawnb58 <shawnb58@BHSCUR149013ITW.kprdsb.schools.local>
2018-12-07 13:53:07 -0500

    adding recs in town

--- renwahsDungeon/src/main/java/cullity/renwahsdungeon/TownController.java
+++ renwahsDungeon/src/main/java/cullity/renwahsdungeon/TownController.java
@@ -5,10 +5,12 @@ package cullity.renwahsdungeon;
  * Date: Dec 4, 2018
  * Made to
  */
-
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.shape.Rectangle;
 
 /**
  * FXML Controller class
@@ -20,11 +22,34 @@ public class TownController implements Initializable {
     /**
      * Initializes the controller class.
      */
-    
-    
+    @FXML
+    private Rectangle recT1;//rec slots in town controller (T)
+    @FXML
+    private Rectangle recT2;
+    @FXML
+    private Rectangle recT3;
+    @FXML
+    private Rectangle recT4;
+    @FXML
+    private Rectangle recT5;
+    @FXML
+    private Rectangle recT6;
+
+    @FXML
+    private void nItem(KeyEvent k) {//nextItem
+        MainApp.nextItem();
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+        MainApp.slot.clear();
+        MainApp.slot.add(recT1);
+        MainApp.slot.add(recT2);
+        MainApp.slot.add(recT3);
+        MainApp.slot.add(recT4);
+        MainApp.slot.add(recT5);
+        MainApp.slot.add(recT6);
+        MainApp.showItems();
+    }
+
 }
